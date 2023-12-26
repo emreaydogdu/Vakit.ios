c1c5d185cb174479c15c60473552f066a90f68cd
eaydgdu <emre.aydogdu@outlook.com>
2023-12-26 23:14:52 +0100

    add settings add moreView

---
+++ Vakit/Views/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  Vakit
+//
+//  Created by Emre Aydogdu on 24.12.23.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}

24a75d9a2e6c636712fe8d8811173deec964c7cc
emreaydogdu <emre.aydogdu@outlook.de>
2024-03-27 23:08:27 +0100

    - ADD create dhikr

--- Vakit/Views/DhikrView.swift
+++ Vakit/Views/DhikrView.swift
@@ -14,7 +14,7 @@ struct DhikrView: View {
 		ZStack(alignment: .top){
 			PatternBG(pattern: false)
 			ScrollView {
-				ForEach(Dhikr.preDefined, id: \.self) { dhikr in
+				ForEach(dhikrs + Dhikr.preDefined, id: \.self) { dhikr in
 					VStack {
 						ZStack(alignment: .topLeading) {
 							RoundedRectangle(cornerRadius: 20, style: .continuous)
@@ -78,6 +78,7 @@ struct DhikrView: View {
 			}
 			.coordinateSpace(name: "scroll")
 			.contentMargins(.top, 80, for: .scrollContent)
+			.contentMargins(.bottom, 90, for: .scrollContent)
 			.onAppear {
 				if dhikrs.isEmpty {
 					//let dhikr2 = Dhikr(id: UUID(), name: "Allahu Akbar",  nameAr: "الله أَكْبَر", count: 99)
@@ -89,34 +90,34 @@ struct DhikrView: View {
 			ToolbarBck(title: "Dhikr", show: $show)
 			VStack{
 				Spacer()
-				ZStack(alignment: .center) {
-					RoundedRectangle(cornerRadius: 16, style: .continuous)
-						.fill(Color(hex: "#C1D2E7"))
-						.shadow(color: .black.opacity(0.15), radius: 24, x: 0, y: 8)
-						.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
-						.padding(5)
-					HStack {
-						Text("Create a new Dhikr")
-							.font(.headline)
-							.fontWeight(.bold)
-							.foregroundColor(Color(hex: "#141414"))
-						Spacer()
-						Image("ic_add")
-							.resizable()
-							.imageScale(.small)
-							.frame(width: 30, height: 30)
-							.foregroundColor(Color(hex: "#141414"))
-					}.padding(.horizontal, 22)
+				Button(action: {add.toggle()}, label: {
+					ZStack(alignment: .center) {
+						RoundedRectangle(cornerRadius: 16, style: .continuous)
+							.fill(Color(hex: "#C1D2E7"))
+							.shadow(color: .black.opacity(0.15), radius: 24, x: 0, y: 8)
+							.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+							.padding(5)
+						HStack {
+							Text("Create a new Dhikr")
+								.font(.headline)
+								.fontWeight(.bold)
+								.foregroundColor(Color(hex: "#141414"))
+							Spacer()
+							Image("ic_add")
+								.resizable()
+								.imageScale(.small)
+								.frame(width: 30, height: 30)
+								.foregroundColor(Color(hex: "#141414"))
+						}.padding(.horizontal, 22)
+					}
+				})
+				.foregroundColor(Color("cardView.title"))
+				.sheet(isPresented: $add) {
+					DhikrAddView()
 				}
+				.padding(.horizontal)
+				.padding(.bottom, 6)
 			}
-			.onTapGesture {
-				add.toggle()
-			}
-			.sheet(isPresented: $add, content: {
-				DhikrAddView()
-			})
-			.padding(.horizontal)
-			.padding(.bottom, 6)
 		}
 		.navigationBarBackButtonHidden(true)
 	}
@@ -129,12 +130,8 @@ struct DhikrCountView: View {
 	
 	var body: some View {
 		ZStack(alignment: .top) {
-			Color("backgroundColor").ignoresSafeArea()
-			Capsule()
-				.fill(Color.secondary)
-				.opacity(0.5)
-				.frame(width: 35, height: 5)
-				.padding(10)
+			PatternBG(pattern: false)
+			Capsule().fill(Color.secondary).frame(width: 35, height: 5).padding(.top, 12)
 			VStack {
 				Text("\(dhikr.count.description)x")
 					.frame(maxWidth: .infinity, alignment: .leading)
@@ -170,11 +167,230 @@ struct DhikrCountView: View {
 	}
 }
 
+struct DhikrAddView: View {
+	
+	@Environment(\.modelContext) var context
+	@Environment(\.dismiss) var dismiss
+	@State private var add = false
+	@State private var preDefined = false
+	@State private var original = ""
+	@State private var translation = ""
+	@State private var amount = ""
+	@State private var show = false
+	@State private var defaultv: CGPoint = .zero
+	@State private var selectedDhikr: Dhikr?
+	
+	var body: some View {
+		ZStack{
+			PatternBG(pattern: false)
+			VStack {
+				Capsule().fill(Color.secondary).frame(width: 35, height: 5).padding(.top, 12)
+				Spacer()
+			}
+			VStack{
+				FormSection(header: "SELECT DHIKR", footer: "Select a predefined Dhikr from the List or create a new one right below") {
+					Button(action: {preDefined.toggle()}, label: {
+						ZStack(alignment: .center) {
+							RoundedRectangle(cornerRadius: 13, style: .continuous)
+								.fill(Color("cardView"))
+								.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+								.padding(5)
+							HStack {
+								Text("Select Dhikr")
+									.font(.headline)
+									.fontWeight(.bold)
+									.foregroundColor(Color("cardView.title"))
+								Spacer()
+								Image("ic_share")
+									.resizable()
+									.imageScale(.small)
+									.frame(width: 30, height: 30)
+									.rotationEffect(.degrees(45))
+									.foregroundColor(Color("cardView.title"))
+							}.padding(.horizontal, 22)
+						}
+						.padding(.horizontal)
+					})
+					.sheet(isPresented: $preDefined){
+						ZStack{
+							PatternBG(pattern: false)
+							VStack {
+								Capsule().fill(Color.secondary).frame(width: 35, height: 5).padding(.top, 12)
+								ScrollView {
+								 ForEach(Dhikr.preDefined, id: \.self) { dhikr in
+									 VStack {
+										 ZStack(alignment: .topLeading) {
+											 RoundedRectangle(cornerRadius: 20, style: .continuous)
+												 .fill(Color("cardView.sub"))
+												 .shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
+											 RoundedRectangle(cornerRadius: 16, style: .continuous)
+												 .fill(Color("cardView"))
+												 .shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
+												 .padding(5)
+											 VStack{
+												 Text(dhikr.nameAr)
+													 .font(.title)
+													 .padding(.top, 12)
+												 Text(dhikr.name)
+													 .font(.headline)
+													 .fontWeight(.bold)
+													 .foregroundColor(Color("textColor"))
+													 .frame(maxWidth: .infinity, alignment: .leading)
+													 .padding(.top, 1)
+											 }
+											 .padding(22)
+										 }
+										 .onTapGesture {
+											 selectedDhikr = dhikr
+											 preDefined.toggle()
+										 }
+									 }
+									 .padding(.horizontal)
+									 .padding(.bottom, 6)
+								 }
+								 .background(GeometryReader { geometry in
+									 Color.clear
+										 .preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scroll")).origin)
+										 .onAppear{
+											 defaultv = geometry.frame(in: .named("scroll")).origin
+										 }
+								 })
+								 .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+									 withAnimation(.linear(duration: 0.1)){
+										 show = value.y < defaultv.y - 30.0
+									 }
+								 }
+							 }
+								Spacer()
+							}
+						}
+					}
+					.onChange(of: selectedDhikr){ dhikr in
+						original = dhikr!.nameAr
+						translation = dhikr!.name
+					}
+				}
+				
+				FormSection(header: "Create Dhikr", footer: "Please provide the original spelling of your dhikr and the translation"){
+					VStack {
+						ZStack(alignment: .center) {
+							RoundedRectangle(cornerRadius: 13, style: .continuous)
+								.fill(Color("cardView"))
+								.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+								.padding(5)
+							HStack {
+								TextField("Original", text: $original)
+									.font(.headline)
+									.foregroundColor(Color("cardView.title"))
+									.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+									.padding(.vertical, 10)
+							}.padding(.horizontal, 22)
+						}
+						.padding(.horizontal)
+						ZStack(alignment: .center) {
+							RoundedRectangle(cornerRadius: 13, style: .continuous)
+								.fill(Color("cardView"))
+								.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+								.padding(5)
+							HStack {
+								TextField("Translation", text: $translation)
+									.font(.headline)
+									.foregroundColor(Color("cardView.title"))
+									.padding(.vertical, 10)
+							}.padding(.horizontal, 22)
+						}
+						.padding(.horizontal)
+					}
+				}
+				
+				FormSection(header: "Choose Amount", footer: ""){
+					VStack {
+						ZStack(alignment: .center) {
+							RoundedRectangle(cornerRadius: 13, style: .continuous)
+								.fill(Color("cardView"))
+								.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+								.padding(5)
+							HStack {
+								TextField("99", text: $amount)
+									.font(.headline)
+									.foregroundColor(Color("cardView.title"))
+									.padding(.vertical, 10)
+							}.padding(.horizontal, 22)
+						}
+						.padding(.horizontal)
+					}
+				}
+				
+				Spacer()
+				Button(action: {
+					let dhikr2 = Dhikr(id: UUID(), name: translation, nameAr: original, count: Int(amount)!)
+					context.insert(dhikr2)
+					
+				}, label: {
+					ZStack(alignment: .center) {
+						RoundedRectangle(cornerRadius: 16, style: .continuous)
+							.fill(Color(hex: "#C1D2E7"))
+							.shadow(color: .black.opacity(0.15), radius: 24, x: 0, y: 8)
+							.frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
+							.padding(5)
+						HStack {
+							Text("Create")
+								.font(.headline)
+								.fontWeight(.bold)
+								.foregroundColor(Color(hex: "#141414"))
+							Spacer()
+							Image("ic_add")
+								.resizable()
+								.imageScale(.small)
+								.frame(width: 30, height: 30)
+								.foregroundColor(Color(hex: "#141414"))
+						}.padding(.horizontal, 22)
+					}
+				})
+				.foregroundColor(Color("cardView.title"))
+				.padding(.horizontal)
+				.padding(.bottom, 6)
+			}
+			.padding(.top, 20)
+		}
+	}
+}
+
+struct FormSection<Content: View>: View {
+	var header: String
+	var footer: String
+	var content: () -> Content
+	
+	var body: some View {
+		Section(
+			header: Text("\(header)".uppercased())
+				.font(.subheadline)
+				.fontWeight(.bold)
+				.foregroundColor(Color("cardView.subtitle"))
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.padding(.leading, 38)
+				.padding(.top, 20),
+			footer: Text("\(footer)")
+				.font(.subheadline)
+				.foregroundColor(Color("cardView.subtitle"))
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.padding(.horizontal, 38))
+		{
+			content()
+		}
+	}
+}
+
 #Preview {
-	DhikrView()
+	DhikrAddView()
 		.modelContainer(for: [Dhikr.self])
 }
 /*
+ #Preview {
+ DhikrView()
+ .modelContainer(for: [Dhikr.self])
+ }
+ 
  #Preview {
  let dhikr = Dhikr(id: UUID(), name: "After Salah", count: 99)
  DhikrCountView(dhikr: dhikr)

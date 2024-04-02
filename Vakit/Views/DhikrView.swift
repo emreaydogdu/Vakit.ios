fec816672c7b49ee6124d7c5c81aadd6744537e4
emreaydogdu <emre.aydogdu@outlook.de>
2024-04-02 19:19:19 +0200

    - CLEAR code

--- Vakit/Views/DhikrView.swift
+++ Vakit/Views/DhikrView.swift
@@ -486,13 +486,3 @@ struct DhikrCountView: View {
  }
  */
 
-extension View {
-	@ViewBuilder
-	func `if`<Content: View>(_ conditional: Bool, content: (Self) -> Content) -> some View {
-		if conditional {
-			content(self)
-		} else {
-			self
-		}
-	}
-}

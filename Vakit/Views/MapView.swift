6a6f09ee0fa25ff0bac4ccacb40f0e6058b64d8e
emreaydogdu <emre.aydogdu@outlook.de>
2024-04-05 00:38:50 +0200

    - FIX sheet dismiss itself

--- Vakit/Views/MapView.swift
+++ Vakit/Views/MapView.swift
@@ -30,92 +30,92 @@ struct MapView: View {
 						.onTapGesture {
 							selectedMosque = location
 						}
-						.sheet(item: $selectedMosque) { mosque in
-							ZStack {
-								PatternBG(pattern: false)
-								VStack {
-									ZStack {
-										RoundedRectangle(cornerRadius: 20, style: .continuous)
-											.fill(Color("cardView.sub"))
-											.shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
-										RoundedRectangle(cornerRadius: 16, style: .continuous)
-											.fill(Color("cardView"))
-											.shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
-											.padding(5)
-										VStack{
-											ShareLink(item: "") {
-												HStack {
-													Text(mosque.name)
-														.font(.headline)
-														.fontWeight(.bold)
-														.foregroundColor(Color("textColor"))
-														.frame(maxWidth: .infinity, alignment: .leading)
-													Image("ic_share")
-														.resizable()
-														.imageScale(.small)
-														.frame(width: 28, height: 28)
-														.foregroundColor(Color("cardView.title"))
-												}
-											}
-											Text(mosque.vicinity)
-												.font(.body)
-												.foregroundColor(Color("subTextColor"))
-												.frame(maxWidth: .infinity, alignment: .leading)
-												.padding(.top, 8)
-												.padding(.bottom)
-												.transition(.opacity)
-											HStack{
-												Spacer()
-												RatingView(maxRating: 5, rating: mosque.rating, starColor: Color("color"))
-												Text("(\(mosque.user_ratings_total))")
-													.font(.body)
-													.foregroundColor(Color("textColor"))
-													.transition(.opacity)
-													.environment(\.layoutDirection, .rightToLeft)
-											}
-											Text("chapter")
-												.font(.headline)
-												.foregroundColor(Color("textColor"))
-												.fontWeight(.bold)
-												.frame(maxWidth: .infinity, alignment: .leading)
-										}
-										.padding(22)
-									}
-									.padding()
-									Spacer()
-									SubmitButton(title: "Navigate", icon: "ic_share") {
-										switch checkMaps() {
-											case 0:
-												isPresentingConfirm.toggle()
-											case 1:
-												openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 0)
-											case 2:
-												openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 1)
-											default:
-												print("Failed")
-										}
-									}
-									.confirmationDialog("Choose your Application",  isPresented: $isPresentingConfirm, titleVisibility: .visible) {
-										Button("Apple Maps") {
-											openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 0)
-										}
-										Button("Google Maps") {
-											openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 1)
-										}
-									}
-								}
-								.padding(.vertical)
-							}
-							.background {
-								GeometryReader { proxy in
-									Color.clear.task { sheetContentHeight = proxy.size.height + 50 }
-								}
-							}.presentationDetents([.height(sheetContentHeight)])
-						}
 					}
 				}
 				UserAnnotation()
 			}
+			.sheet(item: $selectedMosque) { mosque in
+						 ZStack {
+							 PatternBG(pattern: false)
+							 VStack {
+								 ZStack {
+									 RoundedRectangle(cornerRadius: 20, style: .continuous)
+										 .fill(Color("cardView.sub"))
+										 .shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
+									 RoundedRectangle(cornerRadius: 16, style: .continuous)
+										 .fill(Color("cardView"))
+										 .shadow(color: .black.opacity(0.05), radius: 24, x: 0, y: 8)
+										 .padding(5)
+									 VStack{
+										 ShareLink(item: "") {
+											 HStack {
+												 Text(mosque.name)
+													 .font(.headline)
+													 .fontWeight(.bold)
+													 .foregroundColor(Color("textColor"))
+													 .frame(maxWidth: .infinity, alignment: .leading)
+												 Image("ic_share")
+													 .resizable()
+													 .imageScale(.small)
+													 .frame(width: 28, height: 28)
+													 .foregroundColor(Color("cardView.title"))
+											 }
+										 }
+										 Text(mosque.vicinity)
+											 .font(.body)
+											 .foregroundColor(Color("subTextColor"))
+											 .frame(maxWidth: .infinity, alignment: .leading)
+											 .padding(.top, 8)
+											 .padding(.bottom)
+											 .transition(.opacity)
+										 HStack{
+											 Spacer()
+											 RatingView(maxRating: 5, rating: mosque.rating, starColor: Color("color"))
+											 Text("(\(mosque.user_ratings_total))")
+												 .font(.body)
+												 .foregroundColor(Color("textColor"))
+												 .transition(.opacity)
+												 .environment(\.layoutDirection, .rightToLeft)
+										 }
+										 Text("chapter")
+											 .font(.headline)
+											 .foregroundColor(Color("textColor"))
+											 .fontWeight(.bold)
+											 .frame(maxWidth: .infinity, alignment: .leading)
+									 }
+									 .padding(22)
+								 }
+								 .padding()
+								 Spacer()
+								 SubmitButton(title: "Navigate", icon: "ic_share") {
+									 switch checkMaps() {
+										 case 0:
+											 isPresentingConfirm.toggle()
+										 case 1:
+											 openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 0)
+										 case 2:
+											 openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 1)
+										 default:
+											 print("Failed")
+									 }
+								 }
+								 .confirmationDialog("Choose your Application",  isPresented: $isPresentingConfirm, titleVisibility: .visible) {
+									 Button("Apple Maps") {
+										 openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 0)
+									 }
+									 Button("Google Maps") {
+										 openMaps(lat: mosque.geometry.location.lat, lng: mosque.geometry.location.lng, maps: 1)
+									 }
+								 }
+							 }
+							 .padding(.vertical)
+						 }
+						 .background {
+							 GeometryReader { proxy in
+								 Color.clear.task { sheetContentHeight = proxy.size.height + 50 }
+							 }
+						 }.presentationDetents([.height(sheetContentHeight)])
+					 }
 			.mapControls(){
 				MapUserLocationButton().padding(.top, 100)
 				MapPitchToggle()

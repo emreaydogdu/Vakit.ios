f6c0a97ddc3adbcf297d7ddd82849576936cf2db
emreaydogdu <emre.aydogdu@outlook.de>
2024-05-04 22:35:07 +0200

    - TEST

--- Vakit/Views/TestView.swift
+++ Vakit/Views/TestView.swift
@@ -4,14 +4,39 @@ import Foundation
 
 struct TestView: View {
 
+	@AppStorage("themeMode")
+	private var themeMode = "Auto"
+
 	var body: some View {
 		VStack {
-			Button(action: {
-				let prayer = PrayerTimesClass().decodePrayer(key: "prayerTimes1")!
-				print(prayer.getStr(prayer: .fajr))
-			}, label: {
-				Text("Press")
-			})
+			TestV()
+			Picker("Theme", selection: $themeMode) {
+				Text(LocalizedStringKey("Auto")).tag("Auto")
+				Text(LocalizedStringKey("Light")).tag("Light")
+				Text(LocalizedStringKey("Dark")).tag("Dark")
+			}
+			.pickerStyle(.segmented)
+		}
+	}
+}
+
+struct TestV: View {
+	@Environment(\.colorScheme) var colorScheme
+
+	var body: some View {
+		Rectangle()
+			.fill(getColorA())
+			.frame(width: 100, height: 100)
+	}
+
+	func getColorA() -> Color {		
+		switch colorScheme {
+			case .light:
+				return .red
+			case .dark:
+				return .blue
+			default:
+				return .green
 		}
 	}
 }
